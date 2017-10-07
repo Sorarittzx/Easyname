4aeb61633a9408f5c890d00b09a15c2cda33ce60
Sorarittzx <kornz.valentine@gmail.com>
2017-10-07 15:31:56 +0700

    Add fragment to Activity

--- app/src/main/java/chawapee/sorarittzx/easyname/MainActivity.java
+++ app/src/main/java/chawapee/sorarittzx/easyname/MainActivity.java
@@ -3,11 +3,26 @@ package chawapee.sorarittzx.easyname;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import chawapee.sorarittzx.easyname.fragment.MainFragment;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+        //Add Fragment To activity
+        if(savedInstanceState == null){
+          getSupportFragmentManager().beginTransaction()
+                  .add(R.id.contentFragmentMain,new MainFragment())
+                  .commit();
+
+        }
+
+    } //Main Method
+
+
+
+
+}   //Main Class
---
+++ app/src/main/java/chawapee/sorarittzx/easyname/fragment/MainFragment.java
@@ -0,0 +1,26 @@
+package chawapee.sorarittzx.easyname.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import chawapee.sorarittzx.easyname.R;
+
+/**
+ * Created by Sorarittzx on 07-10-2017.
+ */
+
+public class MainFragment extends Fragment{
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_main,container,false);
+
+        return view;
+    }
+}// Main Class
